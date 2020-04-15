aac915d9e082c90d3d89b183f6e62c6b7552d128
Mahdi LARBI <mahdi.larbi@ens.uvsq.fr>
2020-04-15 18:22:41 +0200

    Generate random number #1 , Fermat's test #1

--- src/primality.c
+++ src/primality.c
@@ -10,7 +10,6 @@
 	\param int n : modulo n
 */
 void square_and_multiply(mpz_t R, mpz_t n, char * H){
-	
 	// Initialiser R
 	mpz_t Ri;
 	mpz_init(Ri);
@@ -36,29 +35,81 @@ void square_and_multiply(mpz_t R, mpz_t n, char * H){
 	mpz_clear(Ri);
 }
 
+void random_a(mpz_t a, mpz_t n){
+	// Initialiser
+	gmp_randstate_t state;
+	gmp_randinit_default(state);
+	gmp_printf("%Zd N\n",n);
+
+	// seed
+	gmp_randseed_ui(state, 1234567890);
+
+	/* Pour générer un entier a tel que  1 <  a  <= N-1
+	 * Il faut génerer un nombre aléatoire entre [0, N-3]
+	 * et ajouter 2 au résultat.
+	*/
+	mpz_sub_ui(n,n,3);
+	//
+	mpz_urandomm(a, state, n);
+	mpz_add_ui(a,a,2);
+	gmp_printf("%Zd RAndom\n",a);
+
+	// Free memory
+	gmp_randclear(state);
+}
+
+int fermat_test(mpz_t n, int k){
+	// Initialisation des variables
+	mpz_t a;
+	mpz_t n_1;
+	mpz_init(a);
+	mpz_init(n_1);
+	// Calculer N-1 = exposant
+	mpz_sub_ui(n_1, n, 1);
+	// Convertir exposant en binaire
+	char * H = mpz_get_str(NULL, 2, n_1);
+
+	for (int i = 1; i < k; i++)
+	{	
+		// Générer un entier a aléatoirement
+		random_a(a, n);
+		// Calculer a = a^n-1 mod n
+		square_and_multiply(a, n, H);
+		// Verifier resultat
+		//gmp_printf("%Zd\n",a);
+	}
+
+	//free memory
+	mpz_clear(a);
+	mpz_clear(n_1);
+
+	return 0;
+}
+
 int	main()
 {	
 	//a^exp mod n
 	// Initialisation des variables
-	mpz_t a, n, exp;
-	mpz_init_set_str(a, "4", 10);
-	mpz_init_set_str(exp, "532", 10);
-	mpz_init_set_str(n, "11", 10);
+	mpz_t n;
+	//mpz_init_set_str(a, "15", 10);
+	//mpz_init_set_str(exp, "532", 10);
+	mpz_init_set_str(n, "193", 10);
 
+	//random_a(n, a);
 	// Conversion de l'exposant en binaire
-	char * H = mpz_get_str(NULL, 2, exp);
+	//char * H = mpz_get_str(NULL, 2, exp);
 
 	// Square and multiply
-	square_and_multiply(a, n, H);
+	//square_and_multiply(a, n, H);
 
 	// 
-	gmp_printf("%Zd\n", a);
-
+	//gmp_printf("%Zd\n", a);
+	fermat_test(n, 3);
 
 	// Free memory
-	free(H);
-	mpz_clear(a);
+	//free(H);
+	//mpz_clear(a);
 	mpz_clear(n);
-	mpz_clear(exp);
+	//mpz_clear(exp);
 	return 0;
 }
